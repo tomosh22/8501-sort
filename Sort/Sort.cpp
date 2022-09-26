51202ab7374fb008de31ff43bc994a16d8c50c12
b9014614 <t.o'shaughnessy1@newcastle.ac.uk>
2022-09-26 13:35:30 +0100

    Add project files.

---
+++ Sort/Sort.cpp
@@ -0,0 +1,43 @@
+// Sort.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+int max(int array[10]) {
+    int max = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
+int main()
+{
+    srand(time(0));
+    int array[10];
+    for (int i = 0; i < 10; i++)
+    {
+        array[i] = rand() % 100;
+    }
+    int maxNum = max(array);
+    std::cout << maxNum;
+
+    int* countArray = new int[maxNum];
+    countArray[1] = 99;
+    std::cout << countArray[1];
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
